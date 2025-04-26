8548fd1febc81fa74c1198aeb503bf145da5ae12
Arman <armanstudy9002.com>
2025-04-26 15:11:40 +0300

    3rd edition and cleaning

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ while  game_is_on:
             scoreboard.increase_score()  # Update the score
 
         # Detect collision with walls
-        if snake.segments[0].xcor() > 390 or snake.segments[0].xcor() < -390 or snake.segments[0].ycor() > 290 or snake.segments[0].ycor() < -290:
+        if snake.segments[0].xcor() > 280 or snake.segments[0].xcor() < -280 or snake.segments[0].ycor() > 280 or snake.segments[0].ycor() < -280:
             game_is_on = False
             scoreboard.game_over()
 
